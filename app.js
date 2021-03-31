8c3f85dab9ed0dcc0ab603f2eff0f806a63f1c20
Youssef kafa <48362996+YoussefKafa@users.noreply.github.com>
2021-03-31 23:50:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ const { Book } = require('./model/Book');
 //using
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-
-//routers
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  
-    next();
-  });
 app.use('/api/book',bookRouter);
 app.use('/api/author',authorRouter);
 app.use(express.static(__dirname + '/dist/'));

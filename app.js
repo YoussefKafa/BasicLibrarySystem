8d88d848657d5c36215e82783d8e849e500f738e
Youssef kafa <48362996+YoussefKafa@users.noreply.github.com>
2021-04-01 20:24:55 +0300

    deploying

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const app=express();
 require('dotenv/config');
 const mongoose=require('mongoose');
 const morgan=require('morgan');
-const bookRouter=require('../backend/router/books');
-const authorRouter=require('../backend/router/authors');
-const { Author } = require('./model/Author');
-const { Book } = require('./model/Book');
-
+const bookRouter=require('./router/books.js');
+const authorRouter=require('./router/authors');
+const { Author } = require('./model/Author.js');
+const { Book } = require('./model/Book.js');
+const PORT = process.env.PORT || 5000;
 //using
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use('/api/book',bookRouter);
 app.use('/api/author',authorRouter);
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 app.set('port', (process.env.PORT || 3000));
 //connect to mongodb
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://youssefkafa:123kafa123@libapp.u0dkt.mongodb.net/libApp-database?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true, dbName:'libApp-database' } )
@@ -27,4 +25,4 @@ console.log("mongodb is connected");
     }
 );
 //
-app.listen(process.env.PORT ||5000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
